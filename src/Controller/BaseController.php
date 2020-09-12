23ec7ad4871943085d76f3547aa93379abb44564
Unknown <unknown@example.com>
2020-09-12 11:43:05 +0200

    Mise en place template - menu

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+
+    /**
+     * @Route("/")
+     */
+    public function home()
+    {
+        return $this->render('base.html.twig', []);
+    }
+}
