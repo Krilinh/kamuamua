292b6e51a50be3a7bda6310cb74558485ae32614
Guillaume DEDET <dedetguillaume@gmail.com>
2020-09-13 11:58:26 +0200

    404 + page de contact static

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -17,4 +17,12 @@ class BaseController extends AbstractController
     {
         return $this->render('base.html.twig', []);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig', []);
+    }
 }
